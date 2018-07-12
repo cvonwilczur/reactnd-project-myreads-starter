fcfaa2cda5d44aa2f4c777f6cc15e0820230fa78
cvonwilczur <cvonwilczur@gmail.com>
2018-07-12 12:39:25 -0700

    added logic to handle no thumbnail circumstances

--- src/components/Book.js
+++ src/components/Book.js
@@ -2,13 +2,17 @@ import React from 'react';
 
 
 const Book = (props) => {
-  const { title, authors, imageLinks } = props.book;
+  const { title, authors, imageLinks, shelf } = props.book;
   return (
     <div className="book">
       <div className="book-top">
-        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${imageLinks.thumbnail})` }}></div>
+        { imageLinks && imageLinks.thumbnail ?
+          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${imageLinks.thumbnail})` }}></div>
+         :
+          <div className="book-cover" style={{ width: 128, height: 193}}>No Cover Available</div>
+        }
         <div className="book-shelf-changer">
-          <select onChange={(e) => props.changeShelf(e.target.value, props.book)}>
+          <select value={shelf} onChange={(e) => props.changeShelf(e.target.value, props.book)}>
             <option value="move" disabled>Move to...</option>
             <option value="currentlyReading">Currently Reading</option>
             <option value="wantToRead">Want to Read</option>
